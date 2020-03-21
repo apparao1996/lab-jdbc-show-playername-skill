727f518622117de834ba00ed09327f95e9d71b08
MALLAVALLI APPARAQ <54364675+14761A04E9@users.noreply.github.com>
2020-03-21 22:10:36 +0530

    Comleted

--- src/dao/PlayerDAO.java
+++ src/dao/PlayerDAO.java
@@ -1,1 +1,54 @@
+package dao;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Player;
+import model.Skill;
+import utility.ConnectionManager;
+
+public class PlayerDAO
+{
+	 static List<Player> list=new ArrayList<Player>();
+	public List<Player> getAllPlayers() throws ClassNotFoundException, SQLException, IOException 
+	{
+		Connection con=ConnectionManager.getConnection();
+		 Statement stmt=  con.createStatement();
+			
+			ResultSet rs =stmt.executeQuery("select * from player natural  JOIN skills ");
+		  
+			while(rs.next())
+		    {
+				//System.out.println("hi");
+				long skillId=rs.getLong(1);
+				long playerId=rs.getLong(1);
+				String playerName=rs.getString(3);
+				String playerCountry=rs.getString(4);
+				//System.out.println(playerCountry);
+			
+				long teamId=rs.getLong(5);
+				String skillName=rs.getString(6);
+				
+				Skill skill=new Skill(skillId,skillName);
+				Player player =new Player(playerId,playerName,playerCountry,skill);
+				list.add(player);
+				
+				
+				
+			   
+		    }
+			
+			
+		
+		return list;
+		
+	}
+
+	
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,43 @@
+package dao;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO
+{
+	 public Skill getSkillBylD(Long id) throws ClassNotFoundException, SQLException, IOException 
+	{
+		 Connection con=ConnectionManager.getConnection();
+		 
+		 Statement stmt=  con.createStatement();
+			
+			ResultSet rs =stmt.executeQuery("select * from skills ");
+			Skill skill=new Skill();
+			while(rs.next())
+			{
+				long id1=rs.getLong(1);
+				
+				String name=rs.getString(2);
+				
+				
+				skill.setSkilld(id1);
+				skill.setSkillName(name);
+				//System.out.println(name);
+				//return skill;
+				
+				
+			}
+		
+		return skill;
+		
+	}
+	 
+	 
+	 
+}
